ba571a68079e0edcfdcea81be4f909fc1861f4f9
almuzakir <almuzakir781@gmail.com>
2025-11-17 17:34:16 +0700

    Create register.php

---
+++ public/register.php
@@ -0,0 +1,34 @@
+<?php
+require 'koneksi.php';
+
+$msg = "";
+
+if (isset($_POST['daftar'])) {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $q = mysqli_query($koneksi, "INSERT INTO user VALUES(NULL, '$username', '$password')");
+
+    if ($q) {
+        $msg = "Registrasi berhasil! Silakan login.";
+    } else {
+        $msg = "Gagal mendaftar.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Registrasi</h2>
+
+<?php if ($msg) echo "<p style='color:green'>$msg</p>"; ?>
+
+<form method="POST">
+    <input name="username" placeholder="Username" required><br><br>
+    <input name="password" type="password" placeholder="Password" required><br><br>
+    <button name="daftar">Daftar</button>
+</form>
+
+</body>
+</html>
