5a56b8cef171c54b4aa64fffdf99ec5cf6e5356c
almuzakir <almuzakir781@gmail.com>
2025-11-17 17:34:40 +0700

    Create beranda.php

---
+++ public/beranda.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Selamat datang, <?php echo $_SESSION['username']; ?></h2>
+
+<a href="logout.php">Logout</a>
+</body>
+</html>
